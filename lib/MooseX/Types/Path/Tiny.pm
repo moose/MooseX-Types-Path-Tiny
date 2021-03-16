1f3cb8a6ec2fcc3e076b6e5c9ea0b8c8a45bef5a
Karen Etheridge <ether@cpan.org>
2021-03-16 11:55:04 -0700

    time to switch to 2-column indents

--- lib/MooseX/Types/Path/Tiny.pm
+++ lib/MooseX/Types/Path/Tiny.pm
@@ -3,7 +3,7 @@ use warnings;
 package MooseX::Types::Path::Tiny;
 # ABSTRACT: Path::Tiny types and coercions for Moose
 # KEYWORDS: moose type constraint path filename directory
-# vim: set ts=8 sts=4 sw=4 tw=115 et :
+# vim: set ts=8 sts=2 sw=2 tw=115 et :
 
 our $VERSION = '0.013';
 
